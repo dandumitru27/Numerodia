0b3c231fc82736fad06500756a1f77769601aa42
dandumitru27 <dandumitru27@gmail.com>
2022-10-30 12:08:39 +0200

    Added Div100vh

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
+import Div100vh from 'react-div-100vh'
 
 function App() {
   return (
-    <div>
-      Just play!
-    </div>
+    <Div100vh>
+      <div style={{backgroundColor: 'lightgreen', display: 'flex', height: '100%', textAlign: 'center', padding: '20px', fontSize: 'x-large'}}>
+        Just play!
+      </div>
+    </Div100vh>
   );
 }
 
