9400aca4aa1cec6d9826a2f6357988ae5a9ab698
dandumitru27 <dandumitru27@gmail.com>
2023-05-21 13:49:39 +0300

    Give up on Share API, just copy to clipboard

--- src/lib/shareResult.ts
+++ src/lib/shareResult.ts
@@ -1,14 +1,8 @@
-import { UAParser } from 'ua-parser-js'
 import Puzzle from '../models/puzzle';
 import Hint from '../models/Hint';
 import { formatDateMonth, getSiteName, getSiteUrl, getTrophyType } from '../i18n/translate-methods';
 import Trophy from '../enums/Trophy';
 import Direction from '../enums/Direction';
-
-const webShareApiDeviceTypes: string[] = ['mobile', 'smarttv', 'wearable'];
-const parser = new UAParser();
-const browser = parser.getBrowser();
-const device = parser.getDevice();
 
 export default function shareResult(
   puzzle: Puzzle,
@@ -22,46 +16,18 @@ export default function shareResult(
 
   textToShare += '\n' + getSiteUrl();
 
-  const shareData: ShareData = {
-    text: textToShare
-  };
-
-  let shareSucceeded = false;
-
   try {
-    if (shouldAttemptShare(shareData)) {
-      navigator.share(shareData);
-      shareSucceeded = true;
-    }
-  } catch (error) {
-    shareSucceeded = false;
-  }
-
-  try {
-    if (!shareSucceeded) {
-      if (navigator.clipboard) {
-        navigator.clipboard
-          .writeText(textToShare)
-          .then(handleShareToClipboard)
-          .catch(handleShareFailure)
-      } else {
-        handleShareFailure()
-      }
+    if (navigator.clipboard) {
+      navigator.clipboard
+        .writeText(textToShare)
+        .then(handleShareToClipboard)
+        .catch(handleShareFailure)
+    } else {
+      handleShareFailure()
     }
   } catch (error) {
     handleShareFailure()
   }
-}
-
-const shouldAttemptShare = (shareData: ShareData) => {
-  return (
-    // Firefox Mobile doesn't support share right now
-    browser.name?.toUpperCase().indexOf('FIREFOX') === -1 &&
-    webShareApiDeviceTypes.indexOf(device.type ?? '') !== -1 &&
-    navigator.canShare && // canShare is available only in https
-    navigator.canShare(shareData) &&
-    navigator.share
-  )
 }
 
 const getHintsAsEmojis = (hints: Hint[]): string => {
